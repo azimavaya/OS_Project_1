29a69ef23900bddef94ea08d5fd36f2efe830e5c
Alina Zimavaya <azimavay@highpoint.edu>
2026-03-09 02:06:56 -0400

    Update queue.h

--- Part_B/src/queue.h
+++ Part_B/src/queue.h
@@ -14,3 +14,17 @@ class ReadyQueue {
 private:
     Node* front; //Pointer to the first node in the queue
     Node* rear; //Pointer to the last node in the queue
+public:
+    ReadyQueue();
+    ~ReadyQueue();
+
+    bool isEmpty() const;
+    void enqueue(PCB* p);
+    PCB* dequeue();
+    PCB* peekFront() const;
+    PCB* rmShortestRemain();
+    PCB* rmHighestPriority();
+    void printQueue(ofstream& out) const;
+};
+
+#endif
