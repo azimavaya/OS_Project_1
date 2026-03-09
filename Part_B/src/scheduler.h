4d7bf141b11f06c7797884e9632f64078b692410
Alina Zimavaya <azimavay@highpoint.edu>
2026-03-09 03:46:09 -0400

    Update scheduler.h

--- Part_B/src/scheduler.h
+++ Part_B/src/scheduler.h
@@ -12,6 +12,7 @@ void resetProcesses(PCB* processes, int count);
 bool allTerminated(PCB* processes, int count);
 void admitArrivals(PCB* processes, int count, int currentTime, ReadyQueue& ready);
 void printTrace(ofstream& out, int currentTime, PCB* running, const ReadyQueue& ready);
+void printGanttChart(ofstream& out, string gantt[], int length);
 void simulateFCFS(PCB* processes, int count, const string& outputFile);
 void simulatePriority(PCB* processes, int count, const string& outputFile);
 void simulateSRTF(PCB* processes, int count, const string& outputFile);
