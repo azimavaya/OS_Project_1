00b945d118da19deabe8000d5273bb46fbf26360
Alina Zimavaya <azimavay@highpoint.edu>
2026-03-09 02:03:42 -0400

    Update queue.h
    private created for readyQueue class

--- Part_B/src/queue.h
+++ Part_B/src/queue.h
@@ -4,3 +4,13 @@
 #include "pcb.h"
 #include <fstream>
 using namespace std;
+
+struct Node {
+    PCB* process;
+    Node* next;
+};
+
+class ReadyQueue {
+private:
+    Node* front; //Pointer to the first node in the queue
+    Node* rear; //Pointer to the last node in the queue
