a123c57a1f05c77e720286073d7ef6b64c1e03b0
Alina Zimavaya <azimavay@highpoint.edu>
2026-03-09 03:29:21 -0400

    Update main.cpp

--- Part_B/src/main.cpp
+++ Part_B/src/main.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main() {
-    cout << "TEST TEST TEST" << endl;
+   PCB* processes = nullptr;
+    int count = loadProcesses("input.txt", processes);
+
+    if (count == 0) {
+        cout << "Failed to load." << endl;
+        return 1;
+    }
+    cout << "Loaded " << count << " processes:" << endl;
+
+    for (int i = 0; i < count; i++) {
+        cout << "PID=" << processes[i].pid
+             << " Arr=" << processes[i].arrival
+             << " Burst=" << processes[i].burst
+             << " Rem=" << processes[i].remaining
+             << " Prio=" << processes[i].priority
+             << " State=" << stateToString(processes[i].state)
+             << endl;
+    }
+
+    delete[] processes;
     return 0;
 }
