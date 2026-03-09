00da69a6182a52acd4119c50cc0639bfa3783500
Alina Zimavaya <azimavay@highpoint.edu>
2026-03-09 03:02:38 -0400

    Update scheduler.cpp

--- Part_B/src/scheduler.cpp
+++ Part_B/src/scheduler.cpp
@@ -13,10 +13,49 @@ string stateToString(State s) {
     }
 }
 
+// Load processes from input file into a dynamic PCB aray 
 int loadProcesses(const string& filename, PCB*& processes) {
-    processes = nullptr;
-    return 0;
+    ifstream inFile(filename);
+
+    if (!inFile.is_open()) {
+        cout << "Error: could not open input file." << endl;
+        processes = nullptr;
+        return 0;
+    }
+string header;
+    getline(inFile, header);  // skip header
+
+    int count = 0;
+    string pid;
+    int arrival, burst, priority;
+
+    while (inFile >> pid >> arrival >> burst >> priority) {
+        count++;
+    }
+
+    inFile.close();
+
+    processes = new PCB[count];
+
+    inFile.open(filename);
+    getline(inFile, header);  // skipp header again
+
+    for (int i = 0; i < count; i++) {
+        inFile >> processes[i].pid
+               >> processes[i].arrival
+               >> processes[i].burst
+               >> processes[i].priority;
+
+        processes[i].remaining = processes[i].burst;
+        processes[i].state = NEW;
+        processes[i].startTime = -1;
+        processes[i].completionTime = -1;
+    }
+
+    inFile.close();
+    return count;
 }
+
 
 void resetProcesses(PCB* processes, int count) {
 }
